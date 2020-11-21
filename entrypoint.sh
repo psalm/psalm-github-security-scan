9e957394b61eca10346754fa9acfa6be3ee1e5e7
Matthew Brown <github@muglug.com>
2020-11-20 19:22:37 -0500

    Simplify things

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,17 +7,7 @@ if [ ! -z "$INPUT_REPORT_FILE" ]; then
 fi
 
 if test -f "composer.json"; then
-    IGNORE_PLATFORM_REQS=""
-    if [ "$CHECK_PLATFORM_REQUIREMENTS" = "false" ] || [ "$INPUT_COMPOSER_CHECK_PLATFORM_REQUIREMENTS" = "false" ]; then
-        IGNORE_PLATFORM_REQS="--ignore-platform-reqs"
-    fi
-
-    NO_DEV="--no-dev"
-    if [ "$REQUIRE_DEV" = "true" ] || [ "$INPUT_COMPOSER_REQUIRE_DEV" = "true"  ]; then
-        NO_DEV=""
-    fi
-
-    COMPOSER_COMMAND="composer install --no-scripts --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
+    COMPOSER_COMMAND="composer install --no-scripts --no-progress"
     echo "::group::$COMPOSER_COMMAND"
     $COMPOSER_COMMAND
     echo "::endgroup::"
