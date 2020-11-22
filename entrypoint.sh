f3e6fd9432bc3e44aec078572677ce9d2ef9c287
Matt Brown <github@muglug.com>
2020-11-22 18:48:25 -0500

    Make specific to security analysis

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,13 @@
 #!/bin/sh -l
 set -e
 
-TAINT_ANALYSIS=""
-if [ "$INPUT_SECURITY_ANALYSIS" = "true" ]; then
-    TAINT_ANALYSIS="--taint-analysis"
-fi
-
-REPORT=""
+REPORT_FILE="results.sarif"
 if [ ! -z "$INPUT_REPORT_FILE" ]; then
-    REPORT="--report=$INPUT_REPORT_FILE"
+    REPORT_FILE="$INPUT_REPORT_FILE"
 fi
 
 if test -f "composer.json"; then
-    IGNORE_PLATFORM_REQS=""
-    if [ "$CHECK_PLATFORM_REQUIREMENTS" = "false" ] || [ "$INPUT_COMPOSER_CHECK_PLATFORM_REQUIREMENTS" = "false" ]; then
-        IGNORE_PLATFORM_REQS="--ignore-platform-reqs"
-    fi
-
-    NO_DEV="--no-dev"
-    if [ "$REQUIRE_DEV" = "true" ] || [ "$INPUT_COMPOSER_REQUIRE_DEV" = "true"  ]; then
-        NO_DEV=""
-    fi
-
-    COMPOSER_COMMAND="composer install --no-scripts --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
+    COMPOSER_COMMAND="composer install --no-scripts --no-progress"
     echo "::group::$COMPOSER_COMMAND"
     $COMPOSER_COMMAND
     echo "::endgroup::"
@@ -31,4 +16,4 @@ else
 fi
 
 /composer/vendor/bin/psalm --version
-/composer/vendor/bin/psalm --output-format=github $TAINT_ANALYSIS $REPORT $*
+/composer/vendor/bin/psalm --output-format=github --taint-analysis --report=$REPORT_FILE $*
